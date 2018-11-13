16935b681f75f3aab1af97a2b2769de4e689366e
Domenic Bianchi <dbianchi@uoguelph.ca>
2018-11-12 21:45:48 -0500

    Test

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
-import falcon
+from flask_api import FlaskAPI
 from routes.user import UserRoutes
 
-app = falcon.API()
-userRoutes = UserRoutes()
+app = FlaskAPI(__name__)
+userRoutes = UserRoutes(app)
 app.add_route('/user', userRoutes, suffix='register')
--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,13 +1,17 @@
-import falcon
+from flask_api import FlaskAPI
 import mysql.connector
 
-class UserRoutes(object):
-	def on_get(self, req, res):
-		mydb = mysql.connector.connect(host="localhost", user="root", password="de5ign", port="3306")
-		res.status = falcon.HTTP_200  # This is the default status
-		res.body = ('This is me, Falcon, serving a resource!')
-		mydb.close()
+class UserRoutes():
+	def __init__(self, app):
+		self._app = app
 
+	# def on_get(self, req, res):
+	# 	mydb = mysql.connector.connect(host="localhost", user="root", password="de5ign", port="3306")
+	# 	res.status = falcon.HTTP_200  # This is the default status
+	# 	res.body = ('This is me, Falcon, serving a resource!')
+	# 	mydb.close()
+
+	@_app.route("/user/register", methods=['GET', 'POST'])
 	def on_post_register(self, req, res):
 		if 'username' not in req.params or 'password' not in req.params or 'question' not in req.params or 'answer' not in req.params:
 			resp.body = json.dumps(doc, ensure_ascii=False)
