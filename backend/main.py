4623de62f77c34fd0f2e4b76963e259c97ffba5f
Domenic Bianchi <dbianchi@uoguelph.ca>
2018-11-12 19:53:31 -0500

    Initial commit for auth

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+import Falcon
+from backend import UserRoutes
+
+def main():
+    print("python main function")
+
+
+	app = falcon.API()
+
+	userRoutes = UserRoutes()
+
+	app.add_route('/user', userRoutes)
+
+
+if __name__ == '__main__':
+    main()
