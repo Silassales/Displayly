43ccd2e5c6d65e2e39d9935a7101a30afaf604e5
Domenic Bianchi <dbianchi@uoguelph.ca>
2018-11-12 20:07:37 -0500

    Test

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -6,3 +6,7 @@ class UserRoutes(object):
 		mydb = mysql.connector.connect(host="localhost", user="root", password="de5ign", port="3306")
 		res.status = falcon.HTTP_200  # This is the default status
 		res.body = ('This is me, Falcon, serving a resource!')
+		mydb.close()
+
+	def on_post(self, req, res):
+		print(req.body)
