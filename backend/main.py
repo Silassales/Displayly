eadc034ec874534b0d46bef2424dba2e3e046747
cis3750 <cis3750@cis3750-F18-Template.socs.uoguelph.ca>
2018-11-13 10:50:31 -0500

    Adding get user endpoint

--- backend/main.py
+++ backend/main.py
@@ -3,5 +3,6 @@ from routes.user import UserRoutes
 
 app = falcon.API()
 userRoutes = UserRoutes()
+app.add_route('/user', userRoutes)
 app.add_route('/user/register', userRoutes, suffix='register')
 app.add_route('/user/login', userRoutes, suffix='login')
--- backend/routes/user.py
+++ backend/routes/user.py
@@ -13,10 +13,36 @@ class UserRoutes(object):
 			return None
 		return json.loads(data)
 
+	def decodeToken(self, token):
+		try:
+			options = {'verify_exp': True}
+			return jwt.decode(token, 'secret', verify='True', algorithms=['HS256'], options=options)
+		except (jwt.DecodeError, jwt.ExpiredSignatureError) as err:
+			return None
+
 	def on_get(self, req, res):
-		res.status = falcon.HTTP_200
-		res.body = ('This is me, Falcon, serving a resource!')
-		mydb.close()
+		if req.auth == None:
+			res.status = falcon.HTTP_401
+			res.body = '{"error":"Authorization token required"}'
+		else:
+			tokenContents = self.decodeToken(req.auth)
+
+			if tokenContents == None:
+				res.status = falcon.HTTP_401
+				res.body = '{"error":"Invalid token"}'
+				return
+
+			db = mysql.connector.connect(host="localhost", user="root", password="de5ign", port="3306", db="displayly")
+			cursor = db.cursor()
+			sql = "SELECT Email, Name FROM Users WHERE UserId = %s"
+
+			try:
+				cursor.execute(sql, (tokenContents['userId'],))
+				data = cursor.fetchone()
+				db.close()
+
+				res.status = falcon.HTTP_200
+				res.body = '{"success": true, "userId": %s, "email": %s, "name": %s}' % tokenContents['userId'], data[0], data[1]
 
 	def on_post_register(self, req, res):
 		body = self.getBodyFromRequest(req)
@@ -36,6 +62,7 @@ class UserRoutes(object):
 				db.commit()
 				res.body = '{"success":"New account created."}'
 				res.status = falcon.HTTP_200
+				db.close()
 			except (mysql.connector.errors.IntegrityError) as e:
 				res.body = '{' + '"error":"{}"'.format(e) + '}'
 				res.status = falcon.HTTP_400
@@ -52,6 +79,7 @@ class UserRoutes(object):
 			try:
 				cursor.execute(sql, (body['email'],))
 				data = cursor.fetchone()
+				db.close()
 
 				if data == None or len(data) == 0:
 					res.body = '{"error":"Invalid credentials"}'
@@ -59,7 +87,7 @@ class UserRoutes(object):
 					return
 
 				if bcrypt.checkpw(body['password'].encode('utf8'), data[0].encode('utf8')):
-					token = jwt.encode({'token':data[1], 'exp':datetime.utcnow() + timedelta(seconds=60)}, 'secret', algorithm='HS256').decode('utf8')
+					token = jwt.encode({'userId':data[1], 'exp':datetime.utcnow() + timedelta(seconds=60)}, 'secret', algorithm='HS256').decode('utf8')
 					res.body = '{"success":true, "token":' + '"{}"'.format(token) + '}'
 					res.status = falcon.HTTP_200
 				else:
