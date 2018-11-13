fada4a63a33c969e8bbd4b20dff29cf2b743a2c6
root <root@cis3750-F18-Template.socs.uoguelph.ca>
2018-11-12 23:03:44 -0500

    Bug fixes

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -13,17 +13,17 @@ class UserRoutes(object):
 		raw_json = req.bounded_stream.read()
 		data = raw_json.decode('utf8').replace("'", '"')
 		body = json.loads(data)
-		if 'name' not in body or 'username' not in body or 'password' not in body or 'question' not in body or 'answer' not in body:
+		if 'name' not in body or 'email' not in body or 'password' not in body or 'question' not in body or 'answer' not in body:
 			res.body = '{"error":"Name, email, password, security question and answer are required."}'
 			res.status = falcon.HTTP_400
 		else:
 			db = mysql.connector.connect(host="localhost", user="root", password="de5ign", port="3306", db="displayly")
 			cursor = db.cursor()
-			sql = "INSERT INTO Users (Name, Email, Password, SecurityQuestion, SecurityAnswer) VALUES (%s, %s, %s, %s, %s, %s)"
+			sql = "INSERT INTO Users (Name, Email, Password, SecurityQuestion, SecurityAnswer) VALUES (%s, %s, %s, %s, %s)"
 			hashedPassword = bcrypt.hashpw(body['password'].encode('utf-8'), bcrypt.gensalt())
 			hashedQuestion = bcrypt.hashpw(body['question'].encode('utf-8'), bcrypt.gensalt())
 			hashedAnswer = bcrypt.hashpw(body['answer'].encode('utf-8'), bcrypt.gensalt())
-			cursor.execute(sql, (body['name'], body['email'], body['email'], hashedPassword, hashedQuestion, hashedAnswer))
+			cursor.execute(sql, (body['name'], body['email'], hashedPassword, hashedQuestion, hashedAnswer))
 			db.commit()
 			res.status = falcon.HTTP_200
 
