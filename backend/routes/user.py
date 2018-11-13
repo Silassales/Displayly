b75ef08b2b8837ead9ccfcc3562855e377f2e57d
root <root@cis3750-F18-Template.socs.uoguelph.ca>
2018-11-12 22:42:26 -0500

    Bug fix

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,4 +1,5 @@
 import falcon
+import json
 import mysql.connector
 
 class UserRoutes(object):
@@ -9,15 +10,16 @@ class UserRoutes(object):
 
 	def on_post_register(self, req, res):
 		raw_json = req.bounded_stream.read()
-		body = result.json(raw_json, encoding='utf-8')
+		data = raw_json.decode('utf8').replace("'", '"')
+		body = json.loads(data)
 		if 'username' not in body or 'password' not in body or 'question' not in body or 'answer' not in body:
 			res.body = '{"error":"Username, password, security question and answer are required."}'
 			res.status = falcon.HTTP_400
 		else:
-			db = mysql.connector.connect(host="localhost", user="root", password="de5ign", port="3306")
+			db = mysql.connector.connect(host="localhost", user="root", password="de5ign", port="3306", db="displayly")
 			cursor = db.cursor()
 			sql = "INSERT INTO Users (Email, Password, SecurityQuestion, SecurityAnswer) VALUES (%s, %s, %s, %s)"
-			cursor.execute(sql, (req.params['username'], req.params['password'], req.params['question'], req.params['answer']))
+			cursor.execute(sql, (body['username'], body['password'], body['question'], body['answer']))
 			db.commit()
 			res.status = falcon.HTTP_200
 
