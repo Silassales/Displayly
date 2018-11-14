a4f4cf34d50577c59914f8b4d8c7118b37bedab9
Asad Siddiqui <asad.h.siddiqui@hotmail.com>
2018-11-13 20:34:29 -0500

    Merged and Fixed redirection errors (getting to sign-up)

--- frontend/displayly/src/app/app-routing.module.ts
+++ frontend/displayly/src/app/app-routing.module.ts
@@ -11,8 +11,7 @@ import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {
     path: 'dashboard',
     component: DisplaylyToolbarWrapperComponent,
