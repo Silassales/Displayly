be973903d8e13fd102fcef794610992136ad5fdf
Domenic Bianchi <dbianchi@uoguelph.ca>
2018-11-14 17:59:44 -0500

    Fixing bad indentation

--- backend/routes/token.py
+++ backend/routes/token.py
@@ -3,28 +3,28 @@ import jwt
 from datetime import datetime, timedelta
 
 class TokenRoutes(object):
-    def decodeToken(self, token, expectedResetToken = False):
+	def decodeToken(self, token, expectedResetToken = False):
 		try:
 			options = {'verify_exp': True}
 			decodedToken = jwt.decode(token, 'secret', verify='True', algorithms=['HS256'], options=options)
-	
+
 			if expectedResetToken == False or decodedToken["validForPasswordReset"] == None:
 				return decodedToken
-			return None
-		except (jwt.DecodeError, jwt.ExpiredSignatureError) as err:
-			return None
+				return None
+			except (jwt.DecodeError, jwt.ExpiredSignatureError) as err:
+				return None
 
 	def on_get(self, req, res):
-        if req.auth == None:
+		if req.auth == None:
 			res.status = falcon.HTTP_401
 			res.body = '{"error":"Authorization token required"}'
-		else:
-			tokenContents = self.decodeToken(req.auth)
+			else:
+				tokenContents = self.decodeToken(req.auth)
 
-            if tokenContents == None:
-                res.status = falcon.HTTP_401
-                res.body = '{"error":"Invalid token"}'
-            else:
-                token = jwt.encode({'userId':tokenContents['userId'], 'exp':datetime.utcnow() + timedelta(seconds=1800)}, 'secret', algorithm='HS256').decode('utf8')
-                res.body = '{"success":true, "token":' + '"{}"'.format(token) + '}'
-                res.status = falcon.HTTP_200
+				if tokenContents == None:
+					res.status = falcon.HTTP_401
+					res.body = '{"error":"Invalid token"}'
+				else:
+					token = jwt.encode({'userId':tokenContents['userId'], 'exp':datetime.utcnow() + timedelta(seconds=1800)}, 'secret', algorithm='HS256').decode('utf8')
+					res.body = '{"success":true, "token":' + '"{}"'.format(token) + '}'
+					res.status = falcon.HTTP_200
