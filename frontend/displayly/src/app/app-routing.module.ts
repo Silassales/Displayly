a2a0f6323563b09bfd6c5b44aa742b0891ba9443
Asad Siddiqui <asad.h.siddiqui@hotmail.com>
2018-11-13 19:01:36 -0500

    Added catch-all route

--- frontend/displayly/src/app/app-routing.module.ts
+++ frontend/displayly/src/app/app-routing.module.ts
@@ -21,7 +21,8 @@ const routes: Routes = [
     ]
   },
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: LoginComponent}
 ];
 
 
