1c21516fb3b91b03a2dd569ac77d8cefedce5c78
Domenic Bianchi <dbianchi@uoguelph.ca>
2018-11-14 20:36:49 -0500

    Update

--- backend/routes/scene.py
+++ backend/routes/scene.py
@@ -55,6 +55,7 @@ class SceneRoutes(object):
 			if body == None or 'name' not in body or 'workspaceId' not in body:
 				res.body = '{"error":"Scene name and Workspace ID required."}'
 				res.status = falcon.HTTP_400
+				return
 
 			db = mysql.connector.connect(host="localhost", user="root", password="de5ign", port="3306", db="displayly")
 			cursor = db.cursor()
