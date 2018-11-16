cde66a59b474ba43c4f8eaf849addb9814edab9e
Silas Sales <silas.timothy10@gmail.com>
2018-11-16 13:06:40 -0500

    add template/service for slides

--- frontend/displayly/src/app/slide/slide.component.ts
+++ frontend/displayly/src/app/slide/slide.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SlideService} from '../slide.service';
+import {MatDialog} from '@angular/material';
+// import {CreateSl} from '../create-scene-modal/create-scene-modal.component';
 
 @Component({
   selector: 'app-slide',
@@ -6,10 +10,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slide.component.css']
 })
 export class SlideComponent implements OnInit {
+  adjustedCols: number;
+  adjustedColsList = {
+    xl: 6,
+    md: 3,
+    xs: 1
+  };
+  slides = [];
+  workspaceId: string; // Stores the workspace id from the path
+  loading: boolean;
 
-  constructor() { }
+
+  constructor(private route: ActivatedRoute, private slidesService: SlideService, private dialog: MatDialog, private router: Router) {
+  }
 
   ngOnInit() {
+    if (window.innerWidth >= 1000) {
+      this.adjustedCols = this.adjustedColsList.xl;
+    } else if (window.innerWidth >= 500) {
+      this.adjustedCols = this.adjustedColsList.md;
+    } else {
+      this.adjustedCols = this.adjustedColsList.xs;
+    }
+    this.workspaceId = '61';
+    // this.workspaceId = this.route.snapshot.paramMap.get('workspaceId');
+    // if (!this.workspaceId) { // If we couldn't grab the workspace id from the url, redirect to the dashboard
+    //   this.router.navigate(['dashboard']);
+    //   return;
+    // }
+    this.getSlides();
   }
+
+  onResize(event) {
+    if (event.target.innerWidth >= 1000) {
+      this.adjustedCols = this.adjustedColsList.xl;
+    } else if (event.target.innerWidth >= 500) {
+      this.adjustedCols = this.adjustedColsList.md;
+    } else {
+      this.adjustedCols = this.adjustedColsList.xs;
+    }
+  }
+
+  private getSlides() {
+    this.loading = true;
+    this.slidesService.getSlides(this.workspaceId).subscribe(
+      slides => {
+        this.slides = slides; // Set the slides
+      },
+      err => {
+        // TODO handle error here
+      }, () => this.loading = false
+    );
+  }
+  elementClicked(scene: number) {
+    // TODO: Make this go to something
+  }
+
+  // addElementClicked() {
+  // jack is doing this
+  // }
 
 }
