28e031e760dba7128dabe948661cbec7f2dba1fc
cis3750 <cis3750@cis3750-F18-Template.socs.uoguelph.ca>
2018-11-20 08:43:29 -0500

    fix

--- backend/routes/scene.py
+++ backend/routes/scene.py
@@ -228,7 +228,7 @@ class SceneRoutes(object):
 				json = '{"success": true, "slides": ['
 
 				for slideId in data:
-					json += str(slideId) + ","
+					json += str(slideId[0]) + ","
 
 				res.body = json[:-1] + "]}"
 				res.status = falcon.HTTP_200
